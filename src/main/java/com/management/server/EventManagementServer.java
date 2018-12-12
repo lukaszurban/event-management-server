3cb7339a3c20ad6f63c1e7d85f9c6e1d4dd2e71a
lukaszurban <urbanlukasz94@gmail.com>
2018-12-12 19:14:42 +0100

    Initial commit

---
+++ src/main/java/com/management/server/EventManagementServer.java
@@ -0,0 +1,15 @@
+package com.management.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Łukasz on 25.11.2018
+ */
+@SpringBootApplication
+public class EventManagementServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementServer.class, args);
+    }
+}
